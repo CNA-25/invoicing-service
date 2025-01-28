1e323f966f321a6c3f458189b282a3bb76b3dc2f
xjkg <gripenja@arcada.fi>
2025-01-28 17:37:44 +0200

    Added GET endpoint for /shipments

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import os
+import psycopg
+from psycopg.rows import dict_row
 
 app = FastAPI()
+
+DB_HOST = os.getenv("db_host")
+DB_PORT = os.getenv("db_port")
+DB_NAME = os.getenv("db_name")
+DB_USER = os.getenv("db_user")
+DB_PASSWORD = os.getenv("db_password")
+
+conn_str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 
 @app.get("/")
 def read_root():
     return { "Hello": "Rahti2", "v": "0.4" }
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/shipments")
+def read_shipments():
+    with psycopg.connect(conn_str, row_factory=dict_row) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM shipments")
+            return cur.fetchall()
