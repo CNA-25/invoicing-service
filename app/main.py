8a25e996bbfcc5755a8eab8f3b471f7cef64bb0b
waseniwi <117091617+waseniwi@users.noreply.github.com>
2025-02-13 15:34:21 +0200

    fixed endpoints

--- app/main.py
+++ app/main.py
@@ -24,18 +24,19 @@ def read_orders():
     with psycopg.connect(conn_str, row_factory=dict_row) as conn:
         with conn.cursor() as cur:
             cur.execute("""
-                SELECT o.order_id, o.user_id, o.timestamp, o.order_price,
+                SELECT o.invoice_id, o.user_id, o.timestamp, o.order_price,
                     jsonb_agg(jsonb_build_object(
                         'order_item_id', oi.order_item_id,
                         'product_id', oi.product_id,
                         'amount', oi.amount,
                         'product_price', oi.product_price,
                         'product_name', oi.product_name,
-                        'total_price', oi.total_price
+                        'total_price', oi.total_price,
+                        'invoice_id', oi.invoice_id,
                     )) AS order_items
                 FROM orders o
-                LEFT JOIN order_items oi ON o.order_id = oi.order_id
-                GROUP BY o.order_id
+                LEFT JOIN order_items oi ON o.invoice_id = oi.invoice_id
+                GROUP BY o.invoice_id
             """)
             return cur.fetchall()
         
@@ -43,36 +44,37 @@ def read_orders():
 async def create_order(request: Request):
     try:
         data = await request.json()
-        user_id = data.get("userId")
+        order_id = data.get("order_id")
+        user_id = data.get("user_id")
         timestamp = data.get("timestamp")
-        order_price = data.get("orderPrice")
-        order_items = data.get("orderItems", [])
+        order_price = data.get("order_price")
+        order_items = data.get("order_items", [])
 
-        if not all([user_id, timestamp, order_price, order_items]):
+        if not all([order_id, user_id, timestamp, order_price, order_items]):
             raise HTTPException(status_code=400, detail="Missing required fields")
 
         with psycopg.connect(conn_str) as conn:
             with conn.cursor() as cur:
                 cur.execute(
                     """
-                    INSERT INTO orders (user_id, timestamp, order_price)
-                    VALUES (%s, %s, %s) RETURNING order_id
+                    INSERT INTO orders (order,id, user_id, timestamp, order_price)
+                    VALUES (%s, %s, %s, %s) RETURNING invoice_id
                     """,
-                    (user_id, timestamp, order_price)
+                    (order_id, user_id, timestamp, order_price)
                 )
-                order_id = cur.fetchone()["order_id"]
+                invoice_id = cur.fetchone()["invoice_id"]
 
                 for item in order_items:
                     cur.execute(
                         """
-                        INSERT INTO order_items (order_id, product_id, amount, product_price, product_name)
-                        VALUES (%s, %s, %s, %s, %s)
+                        INSERT INTO order_items (order_item_id, invoice_id, product_id, amount, product_price, product_name)
+                        VALUES (%s, %s, %s, %s, %s, %s)
                         """,
-                        (order_id, item["product_id"], item["amount"], item["product_price"], item["product_name"])
+                        (item["order_item_id"], invoice_id, item["product_id"], item["amount"], item["product_price"], item["product_name"])
                     )
 
                 conn.commit()
 
-        return {"message": "Order created successfully", "order_id": order_id}
+        return {"message": "Order created successfully", "invoice_id": invoice_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
